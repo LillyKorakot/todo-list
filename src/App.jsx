ccbf532743b009c0226dc7ad168c88b553d22c82
Lilly <korakot.jarunongkran@gmail.com>
2023-10-06 23:34:55 +0700

    feat: set up project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className=" text-2xl font-bold flex justify-center items-center">
+        Hello World
+      </div>
+    </>
+  );
+}
+
+export default App;
